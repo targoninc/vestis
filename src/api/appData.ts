638512daa5e261e7d2ddcfc00f883456f709f71e
alexa <alex@targoninc.com>
2025-02-26 00:09:42 +0100

    fix windows build and fix linux data dir

--- src/api/appData.ts
+++ src/api/appData.ts
@@ -1,4 +1,4 @@
 import path from "path";
 
-const userDataPath = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : '/var/local');
+const userDataPath = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : '~/.local/share');
 export const appDataPath = path.join(userDataPath, 'vestis');
